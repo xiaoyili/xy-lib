850833008ab7f90b1e2051ee0cfc0618902db2ff
xiaoyili <us.ericlee@gmail.com>
2013-03-19 16:07:48 -0400

    add usage

--- listTools.py
+++ listTools.py
@@ -16,7 +16,6 @@ def item_count(l, name="value"):
     return [{name: i[1], "count": i[0]} for i in s]
 
 
-
 def chop_list(args, lengths):
     """
     Chopping list into tuples of particular lengths.
@@ -32,8 +31,32 @@ def chop_list(args, lengths):
             r.append(args[idx])
             idx += 1
         else:
-            r.append(tuple(args[idx:idx+l]))
+            r.append(tuple(args[idx:idx + l]))
             idx += l
     assert idx == len(args)
     assert len(r) == len(lengths)
     return r
+
+
+def unique_elements_list_intersection(list1, list2):
+    """
+    Return the unique elements that are in both list1 and list2
+    (repeated elements in listi will not be duplicated in the result).
+    This should run in O(n1+n2) where n1=|list1|, n2=|list2|.
+    """
+    return list(set.intersection(set(list1), set(list2)))
+
+
+if __name__ == '__main__':
+
+    t1 = ['a','a','b','c']
+    # print item_count(t1)
+
+    # print chop_list(t1, (0,2,1))
+
+    t2 = ['a', 'c', 'd']
+
+    # print unique_elements_list_intersection(t1,t2)
+
+    pass
+
